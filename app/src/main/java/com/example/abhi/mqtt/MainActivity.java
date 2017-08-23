b67467ab30164adf449864393af14e84875dd47e
AnujSehgall <anujsehgal.04@gmail.com>
2017-08-23 22:49:32 +0530

    Integrated pub & sub

--- app/src/main/java/com/example/abhi/mqtt/MainActivity.java
+++ app/src/main/java/com/example/abhi/mqtt/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.abhi.mqtt;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+
+
+import helpers.MqttHelper;
+
 
 public class MainActivity extends AppCompatActivity {
+    MqttHelper mqttHelper;
 
+    TextView dataReceived;
     static String HOSTNAME = "tcp://m11.cloudmqtt.com:16201";
     static String USERNAME = "rcduaeoh";
     static String PASSWORD = "hm3O7P_0KiXi";
@@ -23,10 +33,14 @@ public class MainActivity extends AppCompatActivity {
     MqttAndroidClient client;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dataReceived = (TextView) findViewById(R.id.dataReceived);
+
+        startMqtt();
 
         String clientId = MqttClient.generateClientId();
         client = new MqttAndroidClient(this.getApplicationContext(), HOSTNAME, clientId);
@@ -73,4 +87,30 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    private void startMqtt() {
+        mqttHelper = new MqttHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug", mqttMessage.toString());
+                dataReceived.setText(mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
 }
